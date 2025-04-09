96455549be6e29c032dee85565dd48083e7751e6
Tanecodes <tanecodes@gmail.com>
2025-04-10 00:01:45 +1000

    added animation to side text and some minor changes to the layout of my header and hero section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <h1>.My Portfolio.</h1>
+      <h1>MY PORTFOLIO.</h1>
       <nav>
         <ul>
           <li>Home</li>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,10 +8,11 @@ function Hero() {
       <img src={profilePic} alt="profile picture" />
       <div className="introWrapper">
         <h2>Hey i'm Tane</h2>
+        <br />
         <p>
           I'm a self taught front end web developer. I'm currently teaching myself front end
-          and all that comes with it html,CSS,Javascript,React so far. I've made a few small projects with just vanilla html/css/javascript
-          and this portfolio is my first project using React. I'm still in the begining stages of coding
+          and all that comes with it HTML,CSS,Javascript,React. So far i've made a few small projects with just vanilla html/css/javascript
+          and this portfolio is my first project using React. I'm still in the beginning stages of coding
           but my GitHub is pretty active from all my commits showing on the graph squares. If you visit my GitHub you can see.
         </p>  
       </div>
