afb6666f283609df47a0b698caa5cd86e856fef6
Tanecodes <tanecodes@gmail.com>
2025-04-09 17:29:14 +1000

    working on hero section now :)

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import "./Hero.css";
+import profilePic from "../assets/profilePic.jpg";
 
 function Hero() {
   return (
-    <section className="hero">
-      <h2>Hi! i'm Tane</h2>
-      <p>Welcome to my portfolio. I'm ready to code!</p>
-    </section>
+    <div className="hero">
+      <img src={profilePic} alt="profile picture" />
+      <div className="introWrapper">
+        <h2>Hey i'm Tane</h2>
+        <p>
+          I'm a self taught front end web developer. I'm currently teaching myself front end
+          and all that comes with it html,CSS,Javascript,React so far. I've made a few small projects with just vanilla html/css/javascript
+          and this portfolio is my first project using React. I'm still in the begining stages of coding
+          but my GitHub is pretty active from all my commits showing on the graph squares. If you visit my GitHub you can see.
+        </p>  
+      </div>
+    </div>
   );
 }
 
